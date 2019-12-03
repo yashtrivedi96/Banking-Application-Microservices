ed21d02011e7f5803982faf7d9bd11c4d7b16a95
kowshhal97 <54782841+kowshhal97@users.noreply.github.com>
2019-12-03 01:51:31 -0800

    updated transaction

--- Transactions/Main.go
+++ Transactions/Main.go
@@ -13,31 +13,80 @@ type transferRequest struct {
 	EmailID string `json:"email"`
     TransferAmount string `json:"transferAmount"`
 }
-
+type check struct{
+	EmailID string `bson:"email"`
+}
 type error struct{
 	Message string `json:"message"`
 }
+var mongodb_server = "mongodb+srv://nivali:Niv12345@agrifund-fqagq.mongodb.net/Agrifund?retryWrites=true&w=majority"
+var mongodb_database = "Agrifund"
+var mongodb_collection = "accounts"
 
 func main(){
 	router := mux.NewRouter()
 	router.HandleFunc("/transfer",transferPut).Methods("PUT")
-	http.ListenAndServe(":80",router)
+	_ = http.ListenAndServe(":80", router)
 }
 
 func transferPut(w http.ResponseWriter,r *http.Request,){
 
 	var req transferRequest
-	json.NewDecoder(r.Body).Decode(&req)
-	requestBody,err:=json.Marshal(map[string]string{
-			"emailId":req.EmailID,
-			"type":"savings",
-			"operation":"debit",
-			"amount":req.TransferAmount,
-	})
+	var Check check
+	//var result string
+	err:=json.NewDecoder(r.Body).Decode(&req)
 	if(err!=nil){
 		log.Fatal(err)
 	}
-	resp,err:=http.Post("https://i1vv52cmq1.execute-api.us-east-1.amazonaws.com/test","application/json",bytes.NewBuffer(requestBody))
+	Check.EmailID=req.EmailID
+
+
+
+
+
+//	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://nivali:Niv12345@agrifund-fqagq.mongodb.net/Agrifund?retryWrites=true&w=majority"))
+//
+//	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+//	err = client.Connect(ctx)
+//
+//	if(err!=nil){
+//		log.Fatal(err)
+//	}
+//
+//	// Check the connection
+//	err = client.Ping(context.TODO(), nil)
+//
+//	if err != nil {
+//		log.Fatal(err)
+//		fmt.Println("error")
+//	}
+//
+//	fmt.Println("Connected to MongoDB!")
+//	collection := client.Database("Agrifund").Collection("issues")
+//	fmt.Println("after connection")
+//
+//
+//
+//
+//
+//	err = collection.FindOne(context.TODO(), bson.D{{"email", req.EmailID}}).Decode(&result)
+//if(err!=nil){
+//	log.Fatal(err)
+//}
+	obj,err:=json.Marshal(map[string]string{
+		"email":req.EmailID,
+		"type":"savings",
+		"operation":"debit",
+		"amount":req.TransferAmount,
+	})
+	requestBody,err:=json.Marshal(map[string]json.RawMessage{
+		"MessageBody":obj,
+	})
+	fmt.Println(requestBody)
+	if(err!=nil){
+		log.Fatal(err)
+	}
+	resp,err:=http.Post("https://4l0u135eh3.execute-api.us-east-1.amazonaws.com/test/api/send","application/json",bytes.NewBuffer(requestBody))
 	if(err!=nil){
 		log.Fatal(err)
 	}
